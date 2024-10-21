422b6fd837468f151733202fe19c451cd6020e72
Nafi Mahbub <nafimahbub@Nafis-MacBook-Pro.local>
2024-10-21 12:05:48 -0700

    made planner

--- src/components/Remaining.tsx
+++ src/components/Remaining.tsx
@@ -2,20 +2,18 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Remaining = () => {
-  const { expenses } = useContext(AppContext);
-  let budget = 1000;
+  const { expenses, budget } = useContext(AppContext);
 
   const totalExpenses = expenses.reduce((total, item) => {
-    return (total = total + item.cost);
+    return total + item.cost;
   }, 0);
 
-  const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
-
-  // Exercise: Create an alert when Remaining is less than 0.
+  const remaining = budget - totalExpenses;
+  const alertType = remaining < 0 ? "alert-danger" : "alert-success";
 
   return (
     <div className={`alert ${alertType}`}>
-      <span>Remaining: ${budget - totalExpenses}</span>
+      <span>Remaining: ${remaining}</span>
     </div>
   );
 };
